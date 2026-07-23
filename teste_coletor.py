0373e7f9114f938407559b413d60e966c992adfa
Guerragv <monitorimoveisgv@gmail.com>
2026-07-23 07:27:21 -0300

    Organizacao do projeto e limpeza de arquivos temporarios

--- teste_coletor.py
+++
@@ -1,9 +0,0 @@
-from collectors.imoveis_teste import ColetorImoveisTeste
-
-coletor = ColetorImoveisTeste()
-
-resultado = coletor.coletar()
-
-print("\nImóveis encontrados:")
-for imovel in resultado:
-    print(imovel)
--- teste_seguranca.py
+++
@@ -1,8 +0,0 @@
-from collectors.seguranca_imoveis import ColetorSegurancaImoveis
-
-
-coletor = ColetorSegurancaImoveis()
-
-resultado = coletor.coletar()
-
-print(resultado)
